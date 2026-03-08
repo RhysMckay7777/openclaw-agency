44e3e4a8dec38da7e6f1de4c8956126630ba02ea
RhysMckay7777 <rhys@luminaweb3.io>
2026-03-08 23:52:58 +0000

    Update Meta Pixel ID to 890084107238342

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1493876016085995');
+              fbq('init', '890084107238342');
               fbq('track', 'PageView');
             `,
           }}
@@ -103,7 +103,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1493876016085995&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=890084107238342&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
